6d6044dc72893bb4a6fcbe6bd85101979947cf49
kujirahand <web@kujirahand.com>
2019-04-18 19:35:59 +0800

    テスト

---
+++ src/plugin_htmlparser.js
@@ -0,0 +1,174 @@
+// plugin_htmlparser.js
+
+const ERR_ASYNC = '『逐次実行』構文で使ってください。';
+const ERR_PARSER = '『HTML逐次URL開』または『HTMLパース』を実行してHTMLパーサを利用可能にしてください。'
+
+// モジュールローカル
+const htmlObj = {}
+
+// 文字列からDOMを取得して、エラーチェックもする
+function getDom(query) {
+  if (!htmlObj.$) throw new Error(ERR_PARSER)
+  if (typeof query === "string") {
+    const dom = htmlObj.$(query)
+    if (!dom) throw new Error(`クエリ『${query}』が見当たりません。`)
+  } else {
+    if (!query) throw new Error('空のDOMオブジェクトが指定されました。')
+  }
+  return query
+}
+
+const PluginHTMLParser = {
+  '初期化': {
+    type: 'func',
+    josi: [],
+    fn: function (sys) {
+      sys.__htmlparser = null;
+      htmlObj = {}
+    }
+  },
+
+  // @HTMLパーサ(コンソール)
+  'HTML応答ヘッダ': {type: 'const', value: ''}, // @HTMLおうとうへっだ
+  'HTML逐次URL開': { // @任意のURLを開いてパースする。 // @HTMLちくじURLひらく
+    type: 'func',
+    josi: [['を']],
+    fn: function (url, sys) {
+      if (!sys.resolve) throw new Error(ERR_ASYNC)
+      sys.resolveCount++
+      const resolve = sys.resolve
+      const client = require('cheerio-httpcli')
+      client.fetch(url, {}, function (err, $, res) {
+        sys.__v0['HTML応答ヘッダ'] = res.headers
+        sys.__htmlparser = $
+        htmlObj.$ = $
+        resolve()
+      })
+    }
+  },
+  'HTMLパース': { // @任意のHTML文字列をパースする。 // @HTMLぱーす
+    type: 'func',
+    josi: [['を']],
+    fn: function (html, sys) {
+      const client = require('cheerio')
+      const $ = client.load(url)
+      sys.__htmlparser = $
+      htmlObj.$ = $
+      return $
+    }
+  },
+  'DOM要素取得': { // @パース済みHTMLからクエリQに該当するDOMを取得する // @DOMようそしゅとく
+    type: 'func',
+    josi: [['を','の','から']],
+    fn: function (q, sys) {
+      return getDom(q)
+    }
+  },
+  'DOM子要素検索': { // @DOMの子要素Qを取得する // @DOMこようそけんさく
+    type: 'func',
+    josi: [['の','から'],['を']],
+    fn: function (dom, q, sys) {
+      const o = getDom(dom)
+      return o.find(q)
+    }
+  },
+  'DOM子要素全取得': { // @DOMの子要素を全部取得する // @DOMこようそぜんしゅとく
+    type: 'func',
+    josi: [['の','から']],
+    fn: function (dom, sys) {
+      const o = getDom(dom)
+      return o.children()
+    }
+  },
+  'DOM親要素取得': { // @DOMの親要素を取得する // @DOMおやようそしゅとく
+    type: 'func',
+    josi: [['を','の','から']],
+    fn: function (q, sys) {
+      return getDom(q).parent()
+    }
+  },
+  'DOM抽出': { // @DOMからクエリQを利用して合致するものを抽出する // @DOMちゅうしゅつ
+    type: 'func',
+    josi: [['から','の'],['を']],
+    fn: function (dom, q, sys) {
+      const o = getDom(dom)
+      return o.filter(q)
+    }
+  },
+  '属性取得': { // @DOMの属性Kを取得する // @ぞくせいしゅとく
+    type: 'func',
+    josi: [['から','の'],['を']],
+    fn: function (dom, k, sys) {
+      const o = getDom(dom)
+      return o.attr(k)
+    }
+  },
+  'テキスト取得': { // @DOMのテキストを取得する // @てきすとしゅとく
+    type: 'func',
+    josi: [['から','の']],
+    fn: function (dom, sys) {
+      const o = getDom(dom)
+      return o.text()
+    }
+  },
+  'HTML取得': { // @DOMのHTMLを取得する // @HTMLしゅとく
+    type: 'func',
+    josi: [['から','の']],
+    fn: function (dom, sys) {
+      const o = getDom(dom)
+      return o.html()
+    }
+  },
+  'プロパティ取得': { // @DOMのプロパティPROPを取得する // @ぷろぱてぃしゅとく
+    type: 'func',
+    josi: [['から','の'],['を']],
+    fn: function (dom, prop, sys) {
+      const o = getDom(dom)
+      return o.prop(prop)
+    }
+  },
+  '値取得': { // @DOMの値を取得する // @あたいしゅとく
+    type: 'func',
+    josi: [['から','の']],
+    fn: function (dom, sys) {
+      const o = getDom(dom)
+      return o.val()
+    }
+  },
+  'スタイル取得': { // @DOMのスタイルKを取得する // @すたいるしゅとく
+    type: 'func',
+    josi: [['から','の'],['を']],
+    fn: function (dom, k, sys) {
+      const o = getDom(dom)
+      return o.css(k)
+    }
+  },
+  'データ取得': { // @DOMのデータkを取得する // @でーたしゅとく
+    type: 'func',
+    josi: [['から','の'],['を']],
+    fn: function (dom, k, sys) {
+      const o = getDom(dom)
+      return o.data(k)
+    }
+  },
+  'DOM配列変換': { // @DOMを // @でーたしゅとく
+    type: 'func',
+    josi: [['から','の']],
+    fn: function (dom, k, sys) {
+      const o = getDom(dom)
+      return o.serializeArray()
+    }
+  },
+  'タグ名取得': { // @DOMのタグ名を取得する // @たぐめいしゅとく
+    type: 'func',
+    josi: [['から','の']],
+    fn: function (dom, sys) {
+      const o = getDom(dom)
+      return o.tagName
+    }
+  }
+}
+
+
+module.exports = PluginHTMLParser
+
---
+++ test/html-test.js
@@ -0,0 +1,46 @@
+const assert = require('assert')
+const path = require('path')
+const nadesiko3 = require('nadesiko3')
+const NakoCompiler = nadesiko3.compiler
+const PluginNode = nadesiko3.PluginNode
+const PluginHTMLParser = require('../src/plugin_htmlparser.js')
+const assert_func = (a, b) => { assert.equal(a, b) }
+
+describe('htmlparser_test', () => {
+  const nako = new NakoCompiler()
+  nako.addPluginObject('PluginNode', PluginNode)
+  nako.addPluginObject('PluginHTMLParser', PluginHTMLParser)
+  // console.log(nako.gen.plugins)
+  // nako.debug = true
+  nako.setFunc('テスト', [['と'], ['で']], assert_func)
+  const cmp = (code, res) => {
+    if (nako.debug) {
+      console.log('code=' + code)
+    }
+    console.log(nako.runReset('「あ」と表示。'))
+    assert.equal(nako.runReset(code).log, res)
+  }
+  const cmd = (code) => {
+    if (nako.debug) console.log('code=' + code)
+    nako.runReset(code)
+  }
+  // --- test ---
+  it('表示', () => {
+    cmp('3を表示', '3')
+  })
+  // --- テスト ---
+  /*
+  it('逐次実行', (done) => {
+    const url = 'https://nadesi.com/doc3/index.php?plugin_system'
+    global.done = done
+    cmd(
+      '逐次実行\n' +
+      '　先に、「${url}」をHTML逐次URL開く。\n' +
+      '　次に、S=「head > title」のテキスト取得\n' +
+      '　次に、Sと「plugin_system - なでしこ3」でテスト。\n' +
+      '　次に、JS{{{ global.done() }}}\n' +
+      'ここまで\n'
+    )
+  })
+  */
+})
